9cf321c2a72d81943e46f50d512d73992398b928
Milko Lupinacci <mlupinacci@citadelplc.com>
2021-02-17 10:41:10 +0100

    commit updates

--- DllTest/TestDll.cs
+++ DllTest/TestDll.cs
@@ -6,10 +6,11 @@ using System.Text;
 
 namespace DllTest
 {
-    // To Register: 1) cd dll folder, 2) C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm ZipFileComDll.dll /tlb ZipFileComDll.tlb /codebase
+    // To Register: 1) cd dll folder, 2) IMPORTANT: USE 32 bit RegAsm version => C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm DllTest.dll /tlb DllTest.tlb /codebase
 
-    // To Verify registration: 1) cd dll folder, 2) reg query HKLM\SOFTWARE\Classes /s /f ZipFileComDll.dll
+    // To unregister C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /u DllTest.dll
 
+    // To Verify registration: 1) cd dll folder, 2) reg query HKLM\SOFTWARE\Classes /s /f DllTest.dll
 
     [ComVisible(true)]
     [Guid("1B415838-2C45-4DA0-9ACA-B5D4DF84340D")]
